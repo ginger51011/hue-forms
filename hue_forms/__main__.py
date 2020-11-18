0b254b69b10398a7c1af4493e2d5afada20d3a5f
Emil Jonathan Eriksson <eje1999@gmail.com>
2020-11-18 14:10:28 +0100

    Fixed broken commit

--- hue_forms/__main__.py
+++ hue_forms/__main__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 from hue_forms.options import OPTIONS
+from hue_forms.hue_forms import main
 
-if size(OPTIONS) < 1:
+if not OPTIONS:
     raise ValueError("No options set; please edit options.py")
+
+def run():
+    pass
