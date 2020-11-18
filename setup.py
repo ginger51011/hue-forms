ba8db329b16813a1c5dda0d6714e09d01448ea40
Emil Jonathan Eriksson <eje1999@gmail.com>
2020-11-18 14:11:36 +0100

    Fixed broken commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_descritiption = fh.read()
+
+setup(
+    name='hue_forms',
+    version='1.0.1',
+    author='Emil Jonathan Eriksson',
+    author_email='eje1999@gmail.com',
+    description='Control Philips Hue with Google Forms',
+    long_description=long_descritiption,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ginger51011/hue-forms",
+    packages=find_packages(),
+    entry_points = {
+        "console_scripts": [
+            "hue_forms = hue_forms.__main__:run"
+        ]
+    },
+    python_requires=">3.5.0",
+    install_requires=[      # So that pip also installs required packages
+        'requests'
+    ],
+)
